6a7deb683aae837a53136c597f5b6023d0e2061f
gyroflaw <ryo.kanazawa34@gmail.com>
2022-07-03 04:54:26 +0900

    refactor: Check github action

--- src/components/Card/Collection/CollectionCard.tsx
+++ src/components/Card/Collection/CollectionCard.tsx
@@ -3,5 +3,9 @@ import React from 'react'
 import Link from 'next/link'
 
 export default function CollectionCard() {
-  return <Link href="/component" />
+  return (
+    <Link href="/component">
+      <div className="">CollectionCard</div>
+    </Link>
+  )
 }
