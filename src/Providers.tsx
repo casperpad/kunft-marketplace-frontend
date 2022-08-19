4c33fcb6ccda86b7244676d54f352376f55ea51d
gyroflaw <ryo.kanazawa34@gmail.com>
2022-08-19 18:49:06 +0900

    fix: Providers

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -3,16 +3,15 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { SkeletonTheme } from 'react-loading-skeleton'
 import { Provider } from 'react-redux'
 import { Store } from 'redux'
-import { PersistGate } from 'redux-persist/integration/react'
 import styled, { ThemeProvider } from 'styled-components'
 import {
   ModalProvider as StyledModalProvider,
   BaseModalBackground,
 } from 'styled-react-modal'
 
-import { ModalProvider, Spinner } from '@/components'
+import { ModalProvider } from '@/components'
 import CasperWeb3Provider from './providers/CasperWeb3Provider'
-import { persistor } from './store'
+
 import { light } from './theme'
 
 const StyledThemeProvider = (props: any) => {
@@ -37,24 +36,19 @@ export const client = new ApolloClient({
 export default function Providers({ children, store }: ProvdersProps) {
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor} loading={<Spinner />}>
-        <ApolloProvider client={client}>
-          <CasperWeb3Provider>
-            <StyledThemeProvider>
-              <StyledModalProvider backgroundComponent={BlurBackground}>
-                <ModalProvider>
-                  <SkeletonTheme
-                    baseColor="#aaaaaa40"
-                    highlightColor="#aaaaaa20"
-                  >
-                    {children}
-                  </SkeletonTheme>
-                </ModalProvider>
-              </StyledModalProvider>
-            </StyledThemeProvider>
-          </CasperWeb3Provider>
-        </ApolloProvider>
-      </PersistGate>
+      <ApolloProvider client={client}>
+        <CasperWeb3Provider>
+          <StyledThemeProvider>
+            <StyledModalProvider backgroundComponent={BlurBackground}>
+              <ModalProvider>
+                <SkeletonTheme baseColor="#aaaaaa40" highlightColor="#aaaaaa20">
+                  {children}
+                </SkeletonTheme>
+              </ModalProvider>
+            </StyledModalProvider>
+          </StyledThemeProvider>
+        </CasperWeb3Provider>
+      </ApolloProvider>
     </Provider>
   )
 }
